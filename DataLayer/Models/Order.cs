b0a264a447f63ca3bf16aa905a5177836d5fc3c0
dcalancea <calancea.dumitru.md@gmail.com>
2017-05-19 19:30:00 +0300

    WIP

---
+++ DataLayer/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<User> Users { get; set; }
+    }
+}
---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string DossierNr { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OrderNr { get; set; }
+        public DateTime? RegisterDate { get; set; }
+        public DateTime? ResolutionDate { get; set; }
+        public DateTime? Term { get; set; }
+    }
+}
